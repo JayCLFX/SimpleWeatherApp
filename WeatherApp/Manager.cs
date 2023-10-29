ba5f0c7dd78a60eb34f401a7095514565207dbe8
JayCLFX <JayClfx@gmail.com>
2023-10-29 19:32:13 +0100

    Weather App

---
+++ WeatherApp/Manager.cs
@@ -0,0 +1,224 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace WeatherApp
+{
+    static class Manager
+    {
+        static WebClient webClient = new WebClient();
+        public static string json = webClient.DownloadString(Program.Return1stURL());
+        public static dynamic data = JObject.Parse(json);
+
+        public static int GetSunset()
+        {
+            try
+            {
+                int Sunset = data.sys.sunset;
+                return Sunset;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return 0;
+            }
+        }
+
+        public static int GetSunrise()
+        {
+            try
+            {
+                int Sunrise = data.sys.sunrise;
+                return Sunrise;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return 0;
+            }
+        }
+
+        public static string GetCityname()
+        {
+            try
+            {
+                var CityName = data.name;
+                return CityName;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetCountry()
+        {
+            try
+            {
+                var Country = data.sys.country;
+                return Country;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetWindgusts()
+        {
+            try
+            {
+                var WindGusts = data.wind.gust;
+                return WindGusts;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetWinddirection()
+        {
+            try
+            {
+                var WindDirection = data.wind.deg;
+                return WindDirection;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetWindspeed()
+        {
+            try
+            {
+                var WindSpeed = data.wind.speed;
+                return WindSpeed;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetVisibility()
+        {
+            try
+            {
+                var visibility = data.visibility;
+                return visibility;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetHumidity()
+        {
+            try
+            {
+                var Humidity = data.main.humidity;
+                return Humidity;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static string GetPressure()
+        {
+            try
+            {
+                var Pressure = data.main.pressure;
+                return Pressure;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return "Error";
+            }
+        }
+
+        public static int GetTemperatureMax()
+        {
+            try
+            {
+                int Temperature_Max = data.main.temp_max - 273;
+                return Temperature_Max;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return 0;
+            }
+        }
+
+        public static int GetTemperatureMin()
+        {
+            try
+            {
+                int Temperature_Min = data.main.temp_min - 273;
+                return Temperature_Min;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return 0;
+            }
+        }
+
+        public static int GetTemperature()
+        {
+            try
+            {
+                int Temperature = data.main.temp - 273;
+                return Temperature;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return 0;
+            }
+        }
+
+        public static string GetDescription()
+        {
+            try
+            {
+                var Description = data.weather[0].description;
+                return Description;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return "Error";
+            }
+        }
+
+        public static string GetWeather()
+        {
+            try
+            {
+                var Weather = data.weather[0].main;
+                return Weather;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return "Error";
+            }
+        }
+    }
+}
---
+++ WeatherApp/Program.cs
@@ -0,0 +1,167 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace WeatherApp
+{
+    public class Program
+    {
+        static WebClient webClient = new WebClient();
+        static string latitude = "";
+        static string longitude = "";
+        static string ApiKey = "";
+        static string CityName = "";
+
+        static void Main(string[] args)
+        {
+            State("");
+        }
+
+        static void State(string Answer)
+        {
+            Console.WriteLine("State youre Open-Meteo Api Key:");
+            ApiKey = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Choose Mode:");
+            Console.WriteLine("\n1. Longitude / Latitude");
+            Console.WriteLine("\n2. City \n");
+            Answer = Console.ReadLine();
+            CheckInput(Answer);
+        }
+
+        static void CheckInput(string Input)
+        {
+            if (Input != "1" && Input != "2")
+            {
+                Debug.WriteLine(Input);
+                Console.WriteLine("Wrong Answer Specified! \n Return");
+                Thread.Sleep(5000);
+                Console.Clear();
+                State("");
+            }
+
+            if (Input == "1")
+            {
+                Console.Clear();
+                Number1Mode();
+            }
+
+            if (Input == "2")
+            {
+                Console.Clear();
+                Number2Mode();
+            }
+        }
+
+        private static void Number1Mode()
+        {
+            latitude = "0";
+            longitude = "0";
+            Console.WriteLine("State Latitude: \n");
+            latitude = Console.ReadLine();
+            Console.WriteLine("\nState Longitude: \n");
+            longitude = Console.ReadLine();
+            Console.Clear();
+
+            if (latitude != null && longitude != null)
+            {
+                try
+                {
+                    Console.WriteLine(
+                        $"Country : {Manager.GetCountry()} \n" +
+                        $"City Name : {Manager.GetCityname()} \n" +
+                        $"Weather: {Manager.GetWeather()} \n" +
+                        $"Description : {Manager.GetDescription()} \n" +
+                        $"Temperature : {Manager.GetTemperature()} °C \n" +
+                        $"Temperature Minimum : {Manager.GetTemperatureMin()} °C \n" +
+                        $"Temperature Maximum : {Manager.GetTemperatureMax()} °C \n" +
+                        $"Pressure : {Manager.GetPressure()} hpa \n" +
+                        $"Humidity : {Manager.GetHumidity()} q \n" +
+                        $"Visibility : {Manager.GetVisibility()} m \n" +
+                        $"Wind Speed : {Manager.GetWindspeed()} km/h \n" +
+                        $"Wind Direction : {Manager.GetWinddirection()} ° \n" +
+                        $"Wind Gusts : {Manager.GetWindgusts()} km/h + \n" +
+                        $"Sunrise : {Manager.GetSunrise()} UTC \n" +
+                        $"Sunset : {Manager.GetSunset()} UTC \n" +
+                        $"Latitude : {latitude} \n" +
+                        $"Longitude : {longitude} \n"
+
+                        );
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                }
+                Console.ReadLine();
+            }
+        }
+
+        private static void Number2Mode() 
+        {
+            Console.WriteLine("State City Name: \n");
+            CityName = Console.ReadLine();
+            Console.Clear();
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    string json = webClient.DownloadString(Return2ndURL());
+                    dynamic data = JObject.Parse(json);
+
+                    var lat = data.results[0].latitude;
+                    var lon = data.results[0].longitude;
+
+                    if (lat != 0 && lon != 0)
+                    {
+                        latitude = lat;
+                        longitude = lon;
+                        string secjson = webClient.DownloadString(Return1stURL());
+                        dynamic secdata = JObject.Parse(secjson);
+
+                        Console.WriteLine(
+                        $"Country : {Manager.GetCountry()} \n" +
+                        $"City Name : {Manager.GetCityname()} \n" +
+                        $"Weather: {Manager.GetWeather()} \n" +
+                        $"Description : {Manager.GetDescription()} \n" +
+                        $"Temperature : {Manager.GetTemperature()} °C \n" +
+                        $"Temperature Minimum : {Manager.GetTemperatureMin()} °C \n" +
+                        $"Temperature Maximum : {Manager.GetTemperatureMax()} °C \n" +
+                        $"Pressure : {Manager.GetPressure()} hpa \n" +
+                        $"Humidity : {Manager.GetHumidity()} q \n" +
+                        $"Visibility : {Manager.GetVisibility()} m \n" +
+                        $"Wind Speed : {Manager.GetWindspeed()} km/h \n" +
+                        $"Wind Direction : {Manager.GetWinddirection()} ° \n" +
+                        $"Wind Gusts : {Manager.GetWindgusts()} km/h + \n" +
+                        $"Sunrise : {Manager.GetSunrise()} UTC \n" +
+                        $"Sunset : {Manager.GetSunset()} UTC \n" +
+                        $"Latitude : {lat} \n" + 
+                        $"Longitude : {lon} \n"
+
+                        );
+                    }
+                    Console.ReadLine();
+                }
+                catch (WebException ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            }
+        }
+
+        public static string Return1stURL()
+        {
+            string ApiURL = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={ApiKey}";
+            return ApiURL;
+        }
+
+        public static string Return2ndURL()
+        {
+            string secondAPIUrl = $"https://geocoding-api.open-meteo.com/v1/search?name={CityName}&count=10&language=en&format=json";
+            return secondAPIUrl;
+        }
+    }
+}
